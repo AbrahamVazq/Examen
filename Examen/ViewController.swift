8f2953f45bbd8b9af395b2fa5bcb174a7b70f39b
LUIS <luis.enrique.enc@gmail.com>
2022-10-31 18:29:20 -0600

    [ADD] Avance en aplicación de musica

--- Examen/ViewController.swift
+++ Examen/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var launchViewContainer: UIView!{
+        didSet{ self.launchViewContainer.layer.cornerRadius = 20 }
+    }
+    @IBOutlet weak var btnAlbumList: UIButton!
+    @IBOutlet weak var albumImg: UIImageView!
+    @IBOutlet weak var lblAlbumName: UILabel!
+    @IBOutlet weak var lblArtisttName: UILabel!
+    @IBOutlet weak var lblYear: UILabel!
+    @IBOutlet weak var albumListTable: UITableView!{
+        didSet{ self.albumListTable.layer.cornerRadius = 20 }
+    }
+    
+    
+    let arrTDSongs: [String] = ["What You Know","Are We Ready?","I Can Talk","Undercover Martyn","Sun","Bad Decisions","Something Good Can Work","Sleep Alone","Next Year","Changing of the Seasons"]
+    var artistName : String = "Two Door Cinema Club"
+    var albumName : String = "Tourist History"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.albumListTable.delegate = self
+        self.albumListTable.dataSource = self
+        let nibCellFood = UINib(nibName: "SongsTableViewCell", bundle: .main)
+        self.albumListTable.register(nibCellFood, forCellReuseIdentifier: "cellSongs")
     }
-
 
 }
 
+extension ViewController: UITableViewDelegate & UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrTDSongs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellSongs = albumListTable.dequeueReusableCell(withIdentifier: "cellSongs", for: indexPath) as?
+        SongsTableViewCell ?? SongsTableViewCell()
+        cellSongs.lblCellSongName.text = arrTDSongs[indexPath.row]
+        cellSongs.lblCellAlbumName.text = albumName
+        cellSongs.lblcellArtistName.text = artistName
+        cellSongs.CellImageAlbum.image = UIImage(named: "TDCC")
+        return cellSongs
+    }
+    
+}
